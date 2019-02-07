d81e33fa6390f6609abfe3ca876b95c398bef74c
sragvi <nsragvi@gmail.com>
2019-02-06 23:11:01 -0800

    ADDED

---
+++ C/Assignment1/interest/interest/Source.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	int amt = 500000, t = 10, n[] = { 1,6,4,12,365 };
+	float rate = 0, si = 0;
+	printf("Enter the rate\n");
+	scanf_s("%f", &rate);
+	si = amt*t*(rate / 100);
+	printf("The SI %f", si);
+	rate = rate / 100;
+	for (i = 0; i < 5; i++)
+	{
+		si = 0;
+		ai= amt *(1 + rate / n[i])
+		si=pow(si,)
+		P(1 + r / n) ^ nt.
+	}
+}
