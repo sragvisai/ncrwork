cd01964f092e7ecc971fd7377dcb1000f11dd4e8
sragvisai <nsragvi@gmail.com>
2019-02-07 21:56:57 +0530

    MODIFIED REPLACE

--- C/Assignment4/replace/replace/Source.c
+++ C/Assignment4/replace/replace/Source.c
@@ -1,14 +1,33 @@
 #include<stdio.h>
-int replace(char *s)
+#include<stdlib.h>
+#include<string.h>
+int rep(char *s)
 {
-	printf("%s", s);
-	return 1;
+	char *ca;
+	int len = strlen(s), i = 0, c = 0;
+	len++;
+	ca = (char *)malloc(sizeof(char) * len);
+	if (ca == NULL)
+		return -1;
+	else
+	{
+		strcpy_s(ca, len, s);
+		for (i = 0; i < len; i++)
+		{
+			if (s[i] == ' ') {
+				c++;
+				ca[i] = '-';
+			}
+		}
+		printf("%s\n", ca);
+		return c;
+	}
 }
 void main()
 {
-	char *cat = " The cat set";
+	char *cat = "The cat set";
 	int s = 0;
-	s = replace(cat);
+	s = rep(cat);
 	printf("%d\n", s);
 	getch();
 }
