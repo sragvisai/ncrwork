21851002e44eaf41771a29fec1971943279b0f7d
sragvi <nsragvi@gmail.com>
2019-02-07 00:19:04 -0800

    MODIFIED INTEREST

--- C/Assignment1/interest/interest/Source.c
+++ C/Assignment1/interest/interest/Source.c
@@ -1,18 +1,22 @@
 #include<stdio.h>
+#include<math.h>
 void main()
 {
-	int amt = 500000, t = 10, n[] = { 1,6,4,12,365 };
+	int amt = 500000, t = 10, n[] = { 1,2,4,12,365 },i=0;
 	float rate = 0, si = 0;
+	float ai = 0;
 	printf("Enter the rate\n");
 	scanf_s("%f", &rate);
 	si = amt*t*(rate / 100);
 	printf("The SI %f", si);
 	rate = rate / 100;
+	printf("/n");
 	for (i = 0; i < 5; i++)
 	{
-		si = 0;
-		ai= amt *(1 + rate / n[i])
-		si=pow(si,)
-		P(1 + r / n) ^ nt.
+		ai = 0;
+		si = pow((1 + rate / n[i]),(n[i]*t));
+		ai = si*amt;
+			printf(" When compounded %d time a year %f\n", n[i], ai);
 	}
+	getch();
 }
