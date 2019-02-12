c021cdeba61647f8a8f6f41f5bd905268519c613
Bhargavi <jagarapubhargavi@gmail.com>
2019-02-12 00:13:36 -0800

    ADDED THE EXAM FILE

---
+++ C/exam/Project7/Project7/mian.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include"count.h"
+#include"tab2.h"
+void main()
+{
+	int n;
+	char *s = NULL;
+	FILE *fp1, *fp2;
+	fopen_s(&fp1, "sample.txt", "r+");
+	fopen_s(&fp2, "sample2.txt", "w+");
+	printf("Enter the choice\n 1. for counting \n 2.replace tab\n 3.exit\n");
+	scanf_s("%d", &n);
+	switch (n)
+	{
+	case 1: count(fp1);
+		break;
+	case 2:	tab2(fp1, fp2);
+		break;
+	case 3:exit();
+		break;
+	default:
+		break;
+	}
+	getch();
+}
