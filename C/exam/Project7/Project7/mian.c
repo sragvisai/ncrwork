2f16ca6fc69682265eccfa4168edfbc5b0c21257
Bhargavi <jagarapubhargavi@gmail.com>
2019-02-12 02:01:59 -0800

    ADDED REMOVE COMMENTS TO THE EXAM

--- C/exam/Project7/Project7/mian.c
+++ C/exam/Project7/Project7/mian.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 #include"count.h"
 #include"tab2.h"
+#include"comments.h"
 void main()
 {
 	int n;
 	char *s = NULL;
-	FILE *fp1, *fp2;
+	FILE *fp1, *fp2,*fp3;
 	fopen_s(&fp1, "sample.txt", "r+");
 	fopen_s(&fp2, "sample2.txt", "w+");
 	printf("Enter the choice\n 1. for counting \n 2.replace tab\n 3.exit\n");
@@ -16,8 +17,9 @@ void main()
 		break;
 	case 2:	tab2(fp1, fp2);
 		break;
-	case 3:exit();
-		break;
+	case 3:rewind(fp1);
+			comments(fp1,fp2);
+			break;
 	default:
 		break;
 	}
